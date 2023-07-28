1660769061ddd289282759faa42b195c00981574
Abdo <100898720+xAbdoAT@users.noreply.github.com>
2023-07-28 03:05:43 +0200

    Add files via upload

---
+++ g4f/Provider/__init__.py
@@ -0,0 +1,33 @@
+from . import Provider
+from .Providers import (
+    Aichat,
+    Ails,
+    Bard,
+    Better,
+    Bing,
+    ChatgptAi,
+    ChatgptLogin,
+    ChatgptLogin,
+    DeepAi,
+    Easychat,
+    Ezcht,
+    Fakeopen,
+    Forefront,
+    GetGpt,
+    Gravityengine,
+    H2o,
+    hteyun,
+    Liaobots,
+    Lockchat,
+    Mishalsgpt,
+    Phind,
+    Theb,
+    Vercel,
+    Weuseing,
+    Xiaor,
+    Yqcloud,
+    You,
+    Zeabur
+)
+
+Palm = Bard
